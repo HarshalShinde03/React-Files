197ded95a1377b2237dbc8cbd6498ae6f5ce216c
HarshalShinde03 <harsh.shinde70@gmail.com>
2024-05-16 19:10:04 +0530

    Tasks completed

---
+++ 17_practice/src/App.jsx
@@ -0,0 +1,65 @@
+// import { useEffect, useState } from 'react'
+// import axios from 'axios'
+// import './App.css'
+
+// function App() {
+
+//   const [id, setId] = useState(1)
+
+//   function changeId(){
+//     setId(id + 1)
+//   }
+
+//   return (
+//     <>
+//       <button onClick={changeId}>1</button>
+//       <button onClick={changeId}>2</button>
+//       <button onClick={changeId}>3</button>
+//       <button onClick={changeId}>4</button>
+//       <Todo id={id}/>
+//     </>
+//   )
+// }
+
+// function Todo({id}){
+
+//   const [todos, setTodos] = useState({})
+
+
+//   useEffect(()=>{
+//     axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`)
+//     .then(function(res){
+//       setTodos(res.data.todo);
+//     })
+//   },[id])
+//   console.log(todos);
+//   return <div>
+//     <h1>{todos.title}</h1>
+//     <h4>{todos.description}</h4>
+//   </div>
+// }
+// export default App
+
+
+import React from 'react'
+import { useState } from 'react';
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const [val, setVal] = useState(1);
+  return (
+    <div>
+      <input type="text"  onChange={(e)=>setVal(parseInt(e.target.value))}/>
+      {console.log(val)}
+      <br/>
+      Sum is {(val * (val+1))/2}
+      <br />
+
+      <button onClick={function(){
+        setCounter(counter + 1)
+      }}>Counter ({counter})</button>
+    </div>
+  )
+}
+
+export default App
