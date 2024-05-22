55d5ce21f12891ea327e1bbe3800956318d70f40
HarshalShinde03 <harsh.shinde70@gmail.com>
2024-05-22 17:20:27 +0530

    Update

--- 19_prop_drilling/src/App.jsx
+++ 19_prop_drilling/src/App.jsx
@@ -1,35 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useContext, useState } from 'react'
+// import './App.css'
+// import { CountContext } from './context'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <CountContext.Provider value = {count}>
+//         <Count setCount={setCount} />
+//       </CountContext.Provider>
+//     </>
+//   )
+// }
+
+// function Count({setCount}){
+//   const count = useContext(CountContext);
+//   return <div>
+//     {count}
+//     <Buttons setCount={setCount}/> 
+//   </div>
+// }
+  
+// function Buttons({ setCount }){
+//   const count = useContext(CountContext);
+//   return <div>
+//     <button onClick={()=>{
+//       setCount(count+1)
+//     }}>Increase</button>
+
+
+//     <button onClick={()=>{
+//       setCount(count-1)
+//     }}>Decrease</button>
+//   </div>
+// }
+
+// export default App
+
+
+// import React from 'react'
+import React from 'react'
 import './App.css'
+import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil';
+import { countAtom } from './store/atoms/count';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <RecoilRoot>
+          <Count />
+        </RecoilRoot>
     </>
   )
+}
+
+function Count(){
+  console.log("renderrr")
+  return <div>
+    <CountRerender/>
+    <Buttons/> 
+  </div>
+}
+  
+function CountRerender(){
+  const count =useRecoilValue(countAtom);
+  return <div>
+    {count}
+    {count % 2 == 0 ? 'it is even' : ''}
+  </div>
+}
+
+function Buttons(){
+  const [count,setCount] = useRecoilState(countAtom);
+  return <div>
+    <button onClick={()=>{
+      setCount(count+1)
+    }}>Increase</button>
+
+
+    <button onClick={()=>{
+      setCount(count-1)
+    }}>Decrease</button>
+  </div>
 }
 
 export default App
